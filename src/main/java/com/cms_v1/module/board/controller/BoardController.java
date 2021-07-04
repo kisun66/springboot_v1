b72a2bf2deae84c21b32aaeaa3164540d87dcd9e
LeeSeungyoung <sylee@caon.kr>
2021-07-05 05:56:53 +0900

    swagger 추가

--- src/main/java/com/cms_v1/module/board/controller/BoardController.java
+++ src/main/java/com/cms_v1/module/board/controller/BoardController.java
@@ -2,6 +2,9 @@ package com.cms_v1.module.board.controller;
 
 import com.cms_v1.module.board.model.Board;
 import com.cms_v1.module.board.service.BoardService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
@@ -18,6 +21,7 @@ import java.util.List;
  * */
 @RequestMapping("/board")
 @RestController
+@Api(tags = "게시판 리스트 API")
 public class BoardController {
 
     @Autowired
@@ -28,6 +32,7 @@ public class BoardController {
      * @return
      * */
     @GetMapping
+    @ApiOperation(value = "리스트 조회", notes = "게시물 리스트를 조회")
     public List<Board> getList() {
         return boardService.getList();
     };
@@ -39,6 +44,8 @@ public class BoardController {
      * @return
      * */
     @GetMapping("/{BoardNm}")
+    @ApiOperation(value = "상세 조회", notes = "게시물 번호에 해당하는 상세정보를 조회")
+    @ApiImplicitParam(name = "boardNm", value = "게시물 번호", example = "1")
     public Board get(@PathVariable int BoardNm) {
         return boardService.get(BoardNm);
     };
@@ -48,6 +55,7 @@ public class BoardController {
      * @param board
      * */
     @GetMapping("/save")
+    @ApiOperation(value = "생성, 수정", notes = "게시물 번호에 해당하는 데이터를 수정 또는 생성")
     public String save(Board board) {
         boardService.save(board);
         return "save complete";
@@ -59,6 +67,7 @@ public class BoardController {
      * @param BoardNm
      * */
     @GetMapping("/delete/{BoardNm}")
+    @ApiOperation(value = "삭제", notes = "게시물 번호에 해당하는 데이터를 삭제")
     public String delete(@PathVariable int BoardNm) {
         boardService.delete(BoardNm);
         return "delete complete";
