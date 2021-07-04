9b45a980c1384954cca8694be971a689802f1b7b
LeeSeungyoung <sylee@caon.kr>
2021-07-05 05:10:32 +0900

    spring security 설정

--- src/main/java/com/cms_v1/configuration/WebConfiguration.java
+++ src/main/java/com/cms_v1/configuration/WebConfiguration.java
@@ -1,7 +1,10 @@
 package com.cms_v1.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 /**
@@ -11,8 +14,25 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {"classpath:/static/", "classpath:/public/", "classpath:/"
+            , "classpath:/resources/", "classpath:/META-INF/resources/", "classpath:/META-INF/resources/webjars/"};
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        // "/" url Mapping을 forward
+        registry.addViewController("/").setViewName("forward:/index");
+        // 우선순위를 최상으로
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("http://localhost:8080");
     }
+
 }
---
+++ src/main/java/com/cms_v1/configuration/WebSecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.cms_v1.configuration;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    // 정적 자원은 Security 설정을 적용하지 않음
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeRequests()
+                // 나머지 요청은 로그인 요구
+                .anyRequest().permitAll();
+    }
+
+}
